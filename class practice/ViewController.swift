d489b4dd684d3b19a5836c25aed7dc4862bfb00c
Amy Hung <amyhung0123@gmail.com>
2023-03-09 15:51:26 +0800

    Initial Commit

---
+++ class practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  class practice
+//
+//  Created by Amy_H on 3/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
